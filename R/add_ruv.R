ecc7fff2cf3f410a497c0aaca843f2cc1551515b
Ron Keizer <ronkeizer@gmail.com>
2015-10-21 16:49:26 -0700

    Update add_ruv.R

--- R/add_ruv.R
+++ R/add_ruv.R
@@ -2,6 +2,7 @@
 #'
 #' @param x data
 #' @param ruv list with arguments prop, add
+#' @export
 add_ruv <- function (x, ruv) {
   return (x * (1 + rnorm(length(x), 0, ruv$prop)) + rnorm(length(x), 0, ruv$add))
 }
