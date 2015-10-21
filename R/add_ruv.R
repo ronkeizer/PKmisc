cbdd85ed04a915c50b4d56100a323fe7cb4512d4
ronkeizer <ronkeizer@gmail.com>
2015-10-21 16:54:23 -0700

    add exp ruv

--- R/add_ruv.R
+++ R/add_ruv.R
@@ -1,8 +1,17 @@
 #' Add residual variability to data
 #'
 #' @param x data
-#' @param ruv list with arguments prop, add
+#' @param ruv list with arguments prop, add, exp
 #' @export
-add_ruv <- function (x, ruv) {
-  return (x * (1 + rnorm(length(x), 0, ruv$prop)) + rnorm(length(x), 0, ruv$add))
+add_ruv <- function (x, ruv = list()) {
+  if(!is.null(ruv$prop)) {
+    x <- x * (1 + rnorm(length(x), 0, ruv$prop))
+  }
+  if(!is.null(ruv$add)) {
+    x <- x + rnorm(length(x), 0, ruv$add)
+  }
+  if(!is.null(ruv$exp)) {
+    x <- x * exp(rnorm(length(x), 0, ruv$exp))
+  }
+  return (x)
 }
