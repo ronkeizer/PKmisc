17f8eaed14c2adf6b66e5e435464155f732b4ab8
ronkeizer <ronkeizer@gmail.com>
2016-07-05 15:53:21 -0700

    update methods ffm and lbw

--- R/calc_ffm.R
+++ R/calc_ffm.R
@@ -1,31 +1,56 @@
 #' Calculate fat-free mass
 #'
-#' Get an estimate of body-surface area based on weight and height.
+#' Get an estimate of body-surface area based on weight, height, and sex.
 #'
-#' @references Quantification of Lean Bodyweight Sarayut Janmahasatian,1 Stephen B. Duffull,1,2 Susan Ash,3 Leigh C. Ward,4 Nuala M. Byrne5 and Bruce Green1,2
-#' @param weight weight
-#' @param bmi BMI
-#' @param sex sex
+#' References:
+#' `green`: Janmahasatian et al. Clin Pharmacokinet. 2005;44(10):1051-65)
+#' `holford`: McCune J et al. Clin Cancer Res 2013
+#'
+#' @param weight total body weight in kg
+#' @param bmi BMI, only used in `green` method. If `weight` and `height` are both specified, `bmi` will be calculated on-the-fly.
+#' @param height height in cm, only required for `holford` method, can be used instead of `bmi` for `green` method
+#' @param sex sex, either `male` of `female`
+#' @param method estimation method, either `green` (default) or `holford`
 #' @export
 calc_ffm <- function (
   weight = NULL,
   bmi = NULL,
-  sex = NULL
-) {
-  if(is.null(weight) || is.null(bmi) || is.null(sex)) {
-    stop("Equation needs weight, BMI, and sex of patient!")
-  } else {
-    if(!(sex %in% c("male", "female"))) {
-      stop("Sex needs to be either male or female!")
-    }
-    if(sex == "male") {
-      ffm <- (9.27e03 * weight) / ((6.68e03) + 216 * bmi)
-    } else {
-      ffm <- (9.27e03 * weight) / ((8.78e03) + 244 * bmi)
-    }
-    return(list(
-      value = ffm,
-      unit = "kg"
-    ))
+  sex = NULL,
+  height = NULL,
+  method = "green",
+  digits = 1) {
+  methods <- c("green", "holford")
+  if(! method %in% methods) {
+    stop(paste0("Unknown estimation method, please choose from: ", paste(methods, collapse=" ")))
   }
+  if(!(sex %in% c("male", "female"))) {
+    stop("Sex needs to be either male or female!")
+  }
+  if(method == "green") {
+    if(is.null(weight) || (is.null(bmi) & is.null(height)) || is.null(sex)) {
+      stop("Equation needs weight, BMI or height, and sex of patient!")
+    } else {
+      if(is.null(bmi)) {
+        bmi <- calc_bmi(height = height, weight = weight)
+      }
+      if(sex == "male") {
+        ffm <- (9.27e03 * weight) / ((6.68e03) + 216 * bmi)
+      } else {
+        ffm <- (9.27e03 * weight) / ((8.78e03) + 244 * bmi)
+      }
+    }
+  }
+  if(method == "holford") {
+    whs_max <- 42.92
+    whs_50 <- 30.93
+    if(sex == "female") {
+      whs_max <- 37.99
+      whs_max <- 35.98
+    }
+    ffm <- (whs_max * (height/100)^2 * weight) / (whs_50 * (height/100)^2 + weight)
+  }
+  return(list(
+    value = round(ffm,1),
+    unit = "kg"
+  ))
 }
--- R/calc_lbw.R
+++ R/calc_lbw.R
@@ -1,23 +1,77 @@
 #' Calculate lean body weight
-#' from Janmahasatian S et al. Clin Pharmacokinet. 2005;44(10):1051-65
 #'
-#' @param weight weight in kg
+#' Get an estimate of lean body weight (LBW) based on weight, height, and sex.
+#'
+#' Note: technically not the same as fat-free mass, although difference is small.
+#'
+#' References:
+#' `green`: Green and Duffull. Clin Pharmacol Ther 2002;
+#' `james`: Absalom AR et al. Br J Anaesth 2009; 103:26-37
+#' `hume`: Hume R et al. J Clin Pathol. 1966 Jul; 19(4):389-91.
+#' `boer`: Boer P et al. Am J Physiol 1984; 247: F632-5
+#'
+#' @param weight total body weight in kg
 #' @param height height in cm
-#' @param sex either male or female
+#' @param sex sex, either `male` of `female`
+#' @param method estimation method, either `green` (default), `holford`
 #' @export
-calc_lbw <- function(weight=NULL, height=NULL, sex="male") {
-  if(is.null(weight) || is.null(height) || is.null(sex)) {
-    stop("Weight, height, and sex need to be specified!")
+calc_lbw <- function (
+  weight = NULL,
+  bmi = NULL,
+  sex = NULL,
+  height = NULL,
+  method = "green",
+  digits = 1) {
+  methods <- c("green", "holford", "boer", "james", "hume")
+  if(! method %in% methods) {
+    stop(paste0("Unknown estimation method, please choose from: ", paste(methods, collapse=" ")))
   }
-  if(!(tolower(sex) %in% c("male", "female"))) {
-    stop("Sex needs to be either male or female.")
+  if(!(sex %in% c("male", "female"))) {
+    stop("Sex needs to be either male or female!")
   }
-  bmi <- calc_bmi(weight=weight, height=height)
-  # placeholder
-  if(tolower(sex) == "male") {
-    lbw <- (9.27*1000 * weight) / ((6.68*1000) + 216*bmi)
-  } else {
-    lbw <- (9.27*1000 * weight) / ((8.78*1000) + 244*bmi)
+  if(method %in% c("boer", "james", "hume")) {
+    if(is.null(weight) || is.null(height) || is.null(sex)) {
+      stop("Equation needs weight, BMI or height, and sex of patient!")
+    } else {
+      if(method == "boer") {
+        if(sex == "male") {
+          lbm <-  0.407 * weight + 0.237 * height - 19.2
+        } else {
+          lbm <-  0.252 * weight + 0.473 * height - 48.3
+        }
+      }
+      if(method == "hume") {
+        if(sex == "male") {
+          lbm <-  0.3281 * weight + 0.33929 * height - 29.5336
+        } else {
+          lbm <-  0.29569 * weight + 0.41813 * height - 43.2933
+        }
+      }
+      if(method == "james") {
+        if(sex == "male") {
+          lbm <-  1.1 * weight + 128 * (weight/height)^2
+        } else {
+          lbm <-  1.07 * weight + 148 * (weight/height)^2
+        }
+      }
+    }
   }
-  return(lbw)
+  if(method == "green") {
+    if(is.null(weight) || (is.null(bmi) & is.null(height)) || is.null(sex)) {
+      stop("Equation needs weight, BMI or height, and sex of patient!")
+    } else {
+      if(is.null(bmi)) {
+        bmi <- calc_bmi(height = height, weight = weight)
+      }
+      if(sex == "male") {
+        lbm <- (1.1 * weight) - 0.0128 * bmi * weight
+      } else {
+        lbm <- (1.07 * weight) - 0.0148 * bmi * weight
+      }
+    }
+  }
+  return(list(
+    value = round(lbm,1),
+    unit = "kg"
+  ))
 }
