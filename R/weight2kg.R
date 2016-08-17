3b9148012e19426f2b8d7f0e1cf6e6cf48c84ae0
ronkeizer <ronkeizer@gmail.com>
2016-08-17 13:43:50 -0700

    more safety

--- R/weight2kg.R
+++ R/weight2kg.R
@@ -3,12 +3,23 @@
 #' @param value weight in any allowed unit
 #' @param unit unit of weight, one of "lbs", "pound", "pounds", "oz", "ounce", "ounces"
 #' @export
-weight2kg <- function(value, unit) {
-  if (tolower(unit) %in% c("lbs","pound", "pounds")) {
-    value <- value / 2.20462
+weight2kg <- function(value = NULL, unit = NULL) {
+  if(is.null(value)) {
+    stop("No weight value specified.")
   }
-  if (tolower(unit) %in% c("oz", "ounce", "ounces")) {
-    value <- value / 35.274
+  if(!is.null(unit)) {
+    if(tolower(unit) %in% c("lbs", "pound", "pounds", "oz", "ounce", "ounces")) {
+      if (tolower(unit) %in% c("lbs", "pound", "pounds")) {
+        value <- value / 2.20462
+      }
+      if (tolower(unit) %in% c("oz", "ounce", "ounces")) {
+        value <- value / 35.274
+      }
+    } else {
+      warning("Unit not recognized, returning original weight.")
+    }
+  } else {
+    warning("No unit specified, returning original weight.")
   }
   return(value)
 }
